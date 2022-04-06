5ee630c904f198cbe611f34af110d08a941c5d68
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-06 14:48:55 -0500

    using classes in HTML for javascript

---
+++ useJs-class names.js
@@ -0,0 +1,15 @@
+//un js-* nombre de clase para indicar una relación con el 
+//elemento DOM en cuestión.
+
+// html 
+  (`<div class="site-navigation js-site-navigation">`)
+
+
+//the Javasript code
+ const nav = document.querySelector('.js-site-navigation')
+
+
+
+// cualquiera que vea el js-site-navigationnombre de la clase 
+// entenderá que existe una relación con ese elemento DOM en el 
+// código JavaScript.
